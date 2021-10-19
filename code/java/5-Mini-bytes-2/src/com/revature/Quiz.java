b0d88ab2aa1acc3cc64dfcef1932f57cf13c3763
Nag <nagcloudlab@gmail.com>
2021-10-19 12:11:45 -0400

    mini bytes

---
+++ code/java/5-Mini-bytes-2/src/com/revature/Quiz.java
@@ -0,0 +1,16 @@
+package com.revature;
+
+public class Quiz {
+
+    public static void main(String[] args) {
+     display("hello",13,14,'A');
+    }
+
+    public  static void display(String v,int...args){
+
+        char c='A';
+        int i=c;
+
+    }
+
+}
---
+++ code/java/5-Mini-bytes-2/src/com/revature/Quiz2.java
@@ -0,0 +1,12 @@
+package com.revature;
+
+public class Quiz2 {
+    public static void main(String[] args) {
+
+        String s=null;
+        String ss=s.toUpperCase();
+
+        // point - NullPointer Exception is unchecked
+
+    }
+}
