56edaf420512cfcc673724d3889bae132693a976
nagabhushanam <nagabhushanamn@gmail.com>
2021-10-25 12:19:30 -0400

    maven introduction

---
+++ code/java/2-Mini-bytes/2-Java strings/src/com/revature/StringMethods_Ex.java
@@ -0,0 +1,13 @@
+package com.revature;
+
+public class StringMethods_Ex {
+    public static void main(String[] args) {
+
+        // string ==> group of many chars
+
+        String s="revature"; // 0 -7
+        System.out.println(s.charAt(0));
+
+
+    }
+}
