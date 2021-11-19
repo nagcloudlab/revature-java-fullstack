4636b6021c71a00708bd48983e93430ee1576cf8
nagabhushanam <nagabhushanamn@gmail.com>
2021-11-19 12:46:36 -0500

    playing browser apis

--- code/3-javascript/play-with-javascript/src/index.js
+++ code/3-javascript/play-with-javascript/src/index.js
@@ -1,30 +1,112 @@
+import './index.scss'
+console.log("-index.js-")
 
-import './styles/theme.scss'
+// ----------------------------------------
+// using DOM API
+// ----------------------------------------
 
-// const _=require('lodash')
-// or
-import _ from 'lodash'
+// step-1 : query DOM-Elements 
+const alertBox = document.querySelector('.alert-info')
+const greetBtn = document.querySelector(".btn-success")
+const hideBtn = document.querySelector(".btn-danger")
+const showBtn = document.querySelector(".btn-primary")
 
-// const {item1,item2}=require('./hotel/menu'); // cjs
-// -or-
-// import {item1,item2} from './hotel/menu' // es
-// import {item1 as favItem1,item2 as favItem2} from './hotel/menu'
-import mainItem,* as items from './hotel/menu';
+// step-2 : bind event Listeners & react
+greetBtn.addEventListener('click', function () {
+    alertBox.innerText = "good morning mary"
+})
+hideBtn.addEventListener('click', () => {
+    alertBox.style.display = 'none'
+})
+showBtn.addEventListener('click', () => {
+    alertBox.style.display = 'block'
+})
 
 
-let menu1=[
-    'item-2','item-3','item-4'
+// ----------------------------------------
+// using DOM API + Timer Api
+// ----------------------------------------
+
+const images = [
+    './images/1.jpeg',
+    './images/2.jpeg',
+    './images/3.jpeg',
+    './images/4.jpeg',
 ];
 
-let menu2=[
-    'item-3','item-4','item-5',
-];
+const kungfuImgEle = document.getElementById('kungfu-img')
+const startBtn = document.getElementById('start')
+const stopBtn = document.getElementById('stop')
+stopBtn.disabled = true
 
-let differenceMenu=_.difference(menu1,menu2)
-console.log(differenceMenu);
+startBtn.addEventListener('click', e => {
+    startBtn.disabled = true;
+    stopBtn.disabled = false
+    let idx = 0;
+    const intervalId = setInterval(() => {
+        const imagePath = images[idx];
+        idx++;
+        if (idx === images.length) {
+            idx = 0;
+        }
+        kungfuImgEle.src = imagePath;
+    }, 1000)
+    stopBtn.addEventListener('click', e => {
+        clearInterval(intervalId);
+        startBtn.disabled = false;
+        stopBtn.disabled = true
+    })
+})
 
-console.log(mainItem);
-console.log(items.item1);
-console.log(items.item2);
+
+// ----------------------------------------
+// using DOM API + XHR Api
+// ----------------------------------------
+
+const todoListEle = document.getElementById("todo-list")
+
+const topFiveTodosBtn = document.getElementById('top-5-todos')
+const todosLimitListEle = document.getElementById('todos-limit-list')
+const todosLimitInpEle = document.getElementById('todos-limit-inp')
+
+topFiveTodosBtn.addEventListener('click', e => {
+    loadAndRenderTodos()
+})
+todosLimitListEle.addEventListener('change', e => {
+    const limit = e.target.value;
+    if (limit !== '#')
+        loadAndRenderTodos(limit)
+})
+todosLimitInpEle.addEventListener('blur', e => {
+    const limit = e.target.value;
+    if (limit !== '')
+        loadAndRenderTodos(limit)
+})
 
 
+function loadAndRenderTodos(limit = 5) {
+
+    // XHR API to send HTTP Request backend APIS
+    const url = `https://jsonplaceholder.typicode.com/todos?_limit=${limit}`
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, false); // sync request
+    xhr.send()
+
+    const jsonText = xhr.responseText;
+    // parse json-text to javascript array
+    const todos = JSON.parse(jsonText)
+
+    const todoListElements = todos.map(todo => {
+        return `
+        
+            <li class="list-group-item ${todo.completed ? ' bg-success' : ''}">
+                <span class="badge bg-secondary">${todo.id}</span>
+                <span>${todo.title}</span>
+                <span class="badge bg-secondary">${todo.completed}</span>
+            </li>
+
+        `
+    })
+
+    todoListEle.innerHTML = todoListElements.join("");
+}
--- code/3-javascript/play-with-javascript/webpack.config.dev.js
+++ code/3-javascript/play-with-javascript/webpack.config.dev.js
@@ -1,16 +1,25 @@
+
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: './src/index.js',
-  mode:'development',
+  mode: 'development',
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
-  plugins: [new HtmlWebpackPlugin({
-    template:'./src/index.html'
-  })],
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "./src/images", to: "images" },
+      ],
+    }),
+  ],
   module: {
     rules: [
       {
