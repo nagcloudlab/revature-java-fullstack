eaca7808e217581c3765cbd46571366202660ec4
nagabhushanam <nagabhushanamn@gmail.com>
2021-11-30 11:28:38 -0500

    Angular core concepts

--- code/5-web-ui-frameworks/2-NG/shop-ui-v1/README.md
+++ code/5-web-ui-frameworks/2-NG/shop-ui-v1/README.md
@@ -1,4 +1,4 @@
-# ShopWebUiV1
+# ShopUiV1
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.0.3.
 
--- code/5-web-ui-frameworks/2-NG/shop-ui-v1/src/app/app.component.spec.ts
+++ code/5-web-ui-frameworks/2-NG/shop-ui-v1/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'shop-web-ui-v1'`, () => {
+  it(`should have as title 'shop-ui-v1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('shop-web-ui-v1');
+    expect(app.title).toEqual('shop-ui-v1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('shop-web-ui-v1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('shop-ui-v1 app is running!');
   });
 });
