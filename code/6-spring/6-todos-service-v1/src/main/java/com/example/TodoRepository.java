060f0db2b727fd6b575857c29f258ad3c981ceab
nagabhushanam <nagabhushanamn@gmail.com>
2021-12-09 13:05:40 -0500

    todos-service REST api  using spring web

--- code/6-spring/6-todos-service-v1/src/main/java/com/example/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-import com.example.entity.Todo;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.transaction.annotation.Transactional;
-
-@Transactional
-public interface TodoRepository extends CrudRepository<Todo,Integer> {
-}
