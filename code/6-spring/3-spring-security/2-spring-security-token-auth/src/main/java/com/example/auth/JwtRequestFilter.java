05b6258b6165edfed4f6165f4f3f431a3e5c4535
nagabhushanam <nagabhushanamn@gmail.com>
2022-01-05 16:10:41 -0500

    panel doc

--- code/6-spring/3-spring-security/2-spring-security-token-auth/src/main/java/com/example/auth/JwtRequestFilter.java
+++ code/6-spring/3-spring-security/2-spring-security-token-auth/src/main/java/com/example/auth/JwtRequestFilter.java
@@ -41,7 +41,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
         if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
             jwt = authorizationHeader.substring(7);
             username = jwtUtils.extractUsername(jwt);
-        }
+        
 
         try {
             UserDetails userDetails = this.userDetailsService.loadUserByUsername(username);
